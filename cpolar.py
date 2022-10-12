1296df281f4bdf9626b65c8a3142051ab4f90105
lei <lei_fh@foxmail.com>
2022-10-12 14:08:07 +0800

    modify cpolar.py

--- cpolar.py
+++ cpolar.py
@@ -1,32 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-url = 'https://dashboard.cpolar.com/status'
-headers = {
-	'authority':'dashboard.cpolar.com',
-	'method':'GET',
-	'path':'/status',
-	'scheme':'https',
-	'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-	'accept-encoding':'gzip, deflate, br',
-	'accept-language':'zh-CN,zh;q=0.9',
-	'cookie':'_ga=GA1.2.1087364690.1664327971; session=fcf121d1-18d1-4590-988d-7f37654c1c21; _gid=GA1.2.1194350729.1664618732; Hm_lvt_0838dad5461d14f63bdf207a43a54c29=1664327971,1664618614,1664618773,1664619770; _gat_gtag_UA_128397857_1=1; Hm_lpvt_0838dad5461d14f63bdf207a43a54c29=1664676662',
-	'pragma':'no-cache',
-	'referer':'https://dashboard.cpolar.com/get-started',
-	'sec-ch-ua':'"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-	'sec-ch-ua-mobile':'?0',
-	'sec-ch-ua-platform':'Windows',
-	'sec-fetch-dest':'document',
-	'sec-fetch-mode':'navigate',
-	'sec-fetch-site':'same-origin',
-	'sec-fetch-user':'?1',
-	'upgrade-insecure-requests':'1',
-	'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-}
+BASE_URL = 'https://dashboard.cpolar.com'
+LOGIN_URL = urljoin(BASE_URL, '/login')
+STATUS_URL = urljoin(BASE_URL, '/status')
+USERNAME = 'mixinju123@163.com'
+PASSWORD = '7758521MXJ'
+# csrf_token = '1538662349.68##b5aa35f374452a6198004dab20d88b13583c7c2c'
 
-resp = requests.get(url,headers=headers)
+session = requests.Session()
+
+response_login =  session.post(LOGIN_URL, data={
+	'login': USERNAME,
+	'password': PASSWORD,
+	# 'scrf_token': csrf_token
+})
+
+response_status = session.get(STATUS_URL)
 # print(resp)  #结果：<Response [200]>
-resp_text = resp.text
+resp_text = response_status.text
+# print(resp_text)
 text = BeautifulSoup(resp_text, 'html.parser')
 
 tunnels = []
@@ -48,15 +42,50 @@ for td in tables:
 # print(len(tables))
 # print(tables)
 
-res = tunnels
+# res = tunnels
 # for table in tables:
 # 	for tr in table.find_all("tr"):
 # 		for td in tr.find_all("td"):
 # 			print(td.get_text())
 
 # print(resp.text) #拿到页面源代码
-resp.close()  #关掉resp
+response_status.close()  #关掉resp
 
 with open('C:\\Users\\lei\\.ssh\\config', 'w') as f:
     for tunnel in tunnels:
         print(tunnel, file=f)
+
+
+# import requests
+
+# cookies = session.cookies
+# print('Cookies: ', cookies)
+# print('cookies\' type', type(cookies))
+
+# response_status = session.get(STATUS_URL, cookies=cookies)
+# print('Response Status: ', response_status.status_code)
+# print('Response URL: ', response_status.url)
+
+
+# url = 'https://dashboard.cpolar.com/status'
+# headers = {
+# 	'authority':'dashboard.cpolar.com',
+# 	'method':'GET',
+# 	'path':'/status',
+# 	'scheme':'https',
+# 	'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+# 	'accept-encoding':'gzip, deflate, br',
+# 	'accept-language':'zh-CN,zh;q=0.9',
+# 	'cookie':'_ga=GA1.2.1087364690.1664327971; session=fcf121d1-18d1-4590-988d-7f37654c1c21; _gid=GA1.2.1194350729.1664618732; Hm_lvt_0838dad5461d14f63bdf207a43a54c29=1664327971,1664618614,1664618773,1664619770; _gat_gtag_UA_128397857_1=1; Hm_lpvt_0838dad5461d14f63bdf207a43a54c29=1664676662',
+# 	'pragma':'no-cache',
+# 	'referer':'https://dashboard.cpolar.com/get-started',
+# 	'sec-ch-ua':'"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+# 	'sec-ch-ua-mobile':'?0',
+# 	'sec-ch-ua-platform':'Windows',
+# 	'sec-fetch-dest':'document',
+# 	'sec-fetch-mode':'navigate',
+# 	'sec-fetch-site':'same-origin',
+# 	'sec-fetch-user':'?1',
+# 	'upgrade-insecure-requests':'1',
+# 	'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+# }
